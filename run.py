1d6b97457bc09d1f8949f3587e6e4865fa8e773d
Amin Fadaeinejad <51967292+aminfadaei116@users.noreply.github.com>
2023-09-24 14:54:32 -0400

    added the new repo

---
+++ run.py
@@ -0,0 +1,26 @@
+import json
+import math
+from collections import OrderedDict
+import torch
+from torch import nn, Tensor
+from typing import Union, Tuple, List, Iterable, Dict
+import torch.nn.functional as F
+from torch.nn.parameter import Parameter
+from torch.optim import AdamW
+from torch.utils.data import DataLoader
+from scipy.stats import pearsonr, spearmanr
+import numpy as np
+import gzip, csv
+import pandas as pd
+from tqdm.auto import tqdm
+
+torch.manual_seed(0)
+np.random.seed(0)
+
+
+def main():
+    print("hi")
+
+
+if __name__ == "__main__":
+    main()
